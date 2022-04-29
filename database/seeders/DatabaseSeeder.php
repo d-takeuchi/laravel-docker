6c1a0754bd2e169e2c966073b64657268880865f
takeuchi <kuronk.kuro3@gmail.com>
2022-04-29 15:51:16 +0900

    Factoryの利用

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Publisher;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(AuthorsTableSeeder::class);
+        // $this->call(AuthorsTableSeeder::class);
+        Publisher::factory(50)->create();
     }
 }
